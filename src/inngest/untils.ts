6807eedbfb0a6fff0ab7bf3cc1c0c8564ddf81f1
DangKhoa902 <23653401.khoa@student.iuh.edu.vn>
2026-04-14 19:41:16 +0700

    feat(inngest) : set up inngest for background jobs

---
+++ src/inngest/untils.ts
@@ -0,0 +1,40 @@
+import { Sandbox } from "@e2b/code-interpreter";
+import { AgentResult, TextMessage, Message } from "@inngest/agent-kit";
+import { SANDBOX_TIMEOUT } from "./types";
+
+export async function getSandbox(sandboxId:string) {
+    const sandbox = await Sandbox.connect(sandboxId);
+    await sandbox.setTimeout(SANDBOX_TIMEOUT);
+    return sandbox;
+}
+
+export function lastAssistantTextMessageContent(result : AgentResult){
+    const lastAssistantTextMessage = result.output.findLastIndex(
+        (message) => message.role === "assistant",
+    );
+
+    const message = result.output[lastAssistantTextMessage] as 
+    | TextMessage
+    | undefined;
+
+    return message?.content
+    ? typeof message.content === "string"
+        ? message.content
+        : message.content.map((c) => c.text).join("")                
+    : undefined;
+}
+
+
+export const parseAgentOutput = (value: Message[]): string => {
+    const output = value[0];
+
+    if (output.type !== "text") {
+        return "Fragment";
+    }
+
+    if (Array.isArray(output.content)) {
+        return output.content.map((txt) => txt).join("");
+    } else {
+        return output.content;
+    }
+}; 
